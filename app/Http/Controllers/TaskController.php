ba15b6ca60dfbe3fe1337d9fa44ea640845fe2ac
Jesse van Vuuren <32089540+JessevanVuuren@users.noreply.github.com>
2023-03-01 22:04:11 +0100

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\storeTaskRequest;
+use App\Http\Resources\TasksResource;
+use App\Models\Task;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TasksResource::collection(
+            Task::where("user_id", Auth::user() -> id)->get()
+        )->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(storeTaskRequest $request)
+    {
+        $request->validated($request->all());
+
+        $task = Task::create([
+            "user_id" => Auth::user()->id,
+            "name" => $request->name,
+            "description" => $request->description,
+            "priority" => $request->priority, 
+        ]);
+
+        return new TasksResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return $this->isNotAuth($task) ? $this->isNotAuth($task): new TasksResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        if (Auth::user()->id !== $task->user_id) {
+            return $this->error("", "You are not authorized to make this request", 403);
+        }
+
+        $task->update($request->all());
+        return new TasksResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        return $this->isNotAuth($task) ? $this->isNotAuth($task): $task->delete();
+    }
+
+    private function isNotAuth($task)
+    {
+        if (Auth::user()->id !== $task->user_id) {
+            return $this->error("", "You are not authorized to make this request", 403);
+        }
+    }
+}
